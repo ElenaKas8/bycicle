cf51cc8187e125cd3582d4164d08b084baf91cf1
Elena <149805926+ElenaKas8@users.noreply.github.com>
2024-03-21 14:53:36 +0100

    border

--- src/components/Services.js
+++ src/components/Services.js
@@ -1,11 +1,10 @@
 import React from 'react';
 
 const Services = (props) => {
-  const { border, title, backgroundColor } = props;
+  const { title, backgroundColor } = props;
   return (
     <div className='services' style={{ backgroundColor }}>
       <div className='services-content'>
-        <div className='services-border' style={{ border }}></div>
         <h3>{title}</h3>
       </div>
     </div>
